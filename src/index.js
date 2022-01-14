9f338a54dea657c62cff2cb44e39fa07368a3eee
Laguta Mikhail <lagutamihail@gmail.com>
2022-01-14 09:04:59 +0300

    feat: task finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+  let s = String(Math.abs(n));
+  let res ="";
+    for (let i = s.length-1; i>=0; i--){
+      res+=s[i];
+  }
+  return Number(res);
 }
